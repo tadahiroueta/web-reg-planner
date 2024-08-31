9748a764e4ad4a2ac613f5f78e88fd90795e368d
tadahiroueta <tadahiroueta@gmail.com>
2024-08-31 17:52:57 -0500

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,91 @@
-import Image from "next/image";
+import post from 'axios';
+import fs from "fs"
+import { JSDOM } from 'jsdom';
 
-export default function Home() {
+import departments from '../data/departments.json';
+import releaseDates from '../data/releaseDates.json';
+import classProperties from '../data/classProperties.json';
+
+const URL = "https://www.reg.uci.edu/perl/WebSoc";
+const searchParams = {
+  "ShowComments": "on",
+  "ShowFinals": "on",
+  "CancelledCourses": "Exclude",
+  "Submit": "Display Web Results" // Text for dev, Web for prod
+};
+
+
+/**
+ * @returns The upcoming academic term in UC Irvine's code format, according to the release dates.
+ */
+function getTerm(): string | undefined {
+  const now = new Date();
+
+  for (const [term, date] of Object.entries(releaseDates)) 
+    if (now >= new Date(date)) return term;
+}
+
+/**
+ * 
+ * @param html the web results for a department
+ * @returns an array of courses
+ */
+function parseCourses(html: string): any {
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  // let courses: HTMLTableCellElement[] = Array.from(document.querySelectorAll(".CourseTitle"));
+  // return courses.map((element) => (element.cellIndex + element.textContent));
+
+  const courses = [];
+
+  let rows = document.querySelectorAll("tr");
+  var course = Object: {};
+  for (const row of rows) {
+
+    // new course
+    if (row.querySelector(".CourseTitle")) {
+      // save complete course
+      if (Object.keys(course).length !== 0) courses.push(course);
+
+      // start getting a new course
+      const header = row.querySelector(".CourseTitle").textContent;
+      const title = row.querySelector(".CourseTitle > font").textContent;
+      course = { 
+        id: header.split(title)[0].trim(),
+        title, 
+      };
+      continue;
+    }
+
+    // class offered
+    if (row.querySelector("tr[valign='top'] > td[nowrap='nowrap']")) {
+      const data = Array.from(row.querySelectorAll("td"));
+      for (let i = 0; i < data.length; i++) {
+        course[classProperties[i]] = data[i].textContent
+    }
+  
+  }
+  return courses.map((course) => course.data + "\n");
+}
+
+async function scrape() {
+  const term = getTerm();
+
+  for (const department of departments) {
+    const response = await post(URL, { params: { ...searchParams, "YearTerm": term, "Dept": department } });
+  }
+}
+
+export default async function Home() {
+  // scrape();
+  // const { data } = await post(URL, { params: { ...searchParams, "YearTerm": getTerm(), "Dept": "I&C SCI" } });
+  const string = fs.readFileSync("./src/data/sample.html", "utf-8");
+  const courses = parseCourses(string);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className='w-full h-screen p-8' style={{ whiteSpace: 'pre-line' }}>
+      { courses }
     </main>
   );
 }
